ea504836fddf7abfd51822a80fecf6c228b10fa3
T-Troll <rk@whowashere.ru>
2021-08-07 08:14:53 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # alienfan-tools
-Utilities for fan and power control for Alienware notebooks
+Utilities for fan and power control for Alienware notebooks trough direct calls into ACPI BIOS.  
+This is the side step from my [alienfx-tools](https://github.com/T-Troll/alienfx-tools) project, dedicated to fan and power control subsystem, and will be integrated into it later on.
+
+Tools avaliable:
+- `alienfan-cli` - simple fan control command line utility
+
+## Disclamer!
+- This tools utilize low-level ACPI functions access, this can be dangerous for unsupported hardware and can provide system hangs, BSODs and even hardware damage! Use with care, for supported hardware only!
+
+## Known issues
+- **DO NOT** try to run this tools for other gear! It can provide dangerous result!
+- After unsuccessful set, ACPI driver can hang into stopping state. Run `alienfan-cli` without parameters a couple of times to reset it (you should see RPM reading if reset succesful).
+- Boost PRMs increased quite fast, but if you lower boost, it took some time to slow down fans. 
+
+## Requrements
+- Windows 10 x64 OS revision 1706 or higher. Any other OS **Does not supported!**
+- "Test mode" should be enabled. Issue `bcdedit /set testsigning on` command from Administarator command prompt to enable it, then reboot.
+- Supported Alienware hardware.
+
+## Installation
+Unpack tools into folder, run any exe.  
+NB: You should have acpilib.dll and hwacc.sys into the same folder.
+
+## Supported hardware
+- `Alienware m15R1`, `Alienware m17R1`
+
+## `alienfan-cli` usage
+it's a simple CLI fan control utility for now, providing the same functionality as AWCC (well... a little more).
+`m15/17R1` can't control fans directly (well... i'm working about it), so all you can do is set fan boost (More RPM).  
+Run `alienfan-cli [lock [boost1 boost2]]`.  
+- Without parameters it provide current fans RPM (funny, AWCC only show "percent" - maybe because they don't want to reveal maximal RPMs is about 5000 - that's why this model so hot - cheap slow fans!)
+- If lock parameter provided, cli locks/unlocks fan boost (1 - unlock, 0 - lock).
+- If both lock and 2 boost provided, cli set fan boost to values provided (well... setting lock to 0 useless in this case).  
+Boosts can be from 0 to 100 (in percent), 100% provide about +2000 RPM to current temp-based values.  
+Boost1 for CPU fan, Boost2 for GPU one. 
+
+## ToDo:
+- [ ] Temperature sensors reading
+- [ ] Eliminate "Test mode" requirement
+- [ ] Additional hardware support (thanks for ACPI dumps, provided by `alienfx-tools` community!):
+  - [ ] `Alienware m15R4`
+  - [ ] `Alienware m15R3`
+  - [ ] `Alienware 17R3`
+  - [ ] `Alienware 13R2`
+  - [ ] `Aurora R7, R8` (also FX support for R7 - this model have it into ACPI!)
+- [ ] Temp-RPM curves and indirect RPM control
+- [ ] CPU power limit control
+- [ ] Dynamic power distribution
+- [ ] `alienfx-gui` integration
+
+## Tools Used
+* Visual Studio Community 2019
+
+## License
+MIT. You can use these tools for any non-commercial or commercial use, modify it any way - supposing you provide a link to this page from you product page and mention me as the one of authors.
+
+## Credits
+Idea, code and hardware support by T-Troll.  
+ACPI SDK and driver based on kdshk's [WindowsHwAccess](https://github.com/kdshk/WindowsHwAccess).  
+Special thanks to [DavidLapous](https://github.com/DavidLapous) for inspiration and advices!
+
+
---
+++ alienfan-cli/alienfan-cli.cpp
@@ -0,0 +1,134 @@
+// alienfan-tools.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "acpilib.h"
+#include "acpi.h"
+
+//#pragma comment(lib, "AcpiLib.lib")
+
+using namespace std;
+
+void DumpAcpi(ACPI_NS_DATA data) {
+    int offset = 0;
+    string fullname;
+    ACPI_NAMESPACE* curData = data.pAcpiNS;
+    for (int i = 0; i < data.uCount; i++) {
+        //for (int j = 0; j < offset; j++)
+        //    cout << "|";
+        string mname = (char*)curData->MethodName;
+        mname.resize(4);
+        cout << fullname << mname << endl;
+        if (curData->pChild) {
+            // need to go deep...
+            if (offset > 0)
+                fullname += mname;
+            curData = curData->pChild;
+            offset++;
+        } else
+            while (curData && !curData->pNext) {
+                // going up to tree
+                curData = curData->pParent;
+                offset--;
+                if (offset > 0)
+                    fullname.resize((offset -1)* 4);
+            }
+            if (curData) {
+                curData = curData->pNext;
+            } else
+                break;
+    }
+}
+
+void GetRPM() {
+    ACPI_EVAL_OUTPUT_BUFFER* res, *res2;
+    res = (ACPI_EVAL_OUTPUT_BUFFER*) EvalAcpiNSArgOutput((TCHAR*) TEXT("\\____SB_AMW1FNSR"), (ACPI_EVAL_INPUT_BUFFER_COMPLEX*) PutIntArg(NULL, 0x32));
+    if (res)
+        cout << "RPM1 is " << res->Argument[0].Argument << ", ";
+    res2 = (ACPI_EVAL_OUTPUT_BUFFER*) EvalAcpiNSArgOutput((TCHAR*) TEXT("\\____SB_AMW1FNSR"), (ACPI_EVAL_INPUT_BUFFER_COMPLEX*) PutIntArg(NULL, 0x33));
+    if (res2)
+        cout << "RPM2 is " << res2->Argument[0].Argument << endl;
+}
+
+void Usage() {
+    cout << "Usage: alienfan-cli [lock [boost1 boost2]]" << endl
+        << "\tLock - 1 off, 0 on" << endl
+        << "\tBoost - 0..100 (in percent)" << endl
+        << "Running without parameters reveal current fans RPMs" << endl;
+}
+
+int main(int argc, char* argv[])
+{
+    std::cout << "AlienFan-cli v0.0.1.0\n";
+
+    ACPI_NS_DATA data = {0};
+    HANDLE acc = OpenAcpiService();
+    if (acc) {
+        OpenAcpiDevice(acc);
+        QueryAcpiNS(acc, &data, 0xc1);
+        ACPI_EVAL_OUTPUT_BUFFER* res, *res2;
+        USHORT nType = ACPI_TYPE_INTEGER;
+        switch (argc) {
+        case 1: // show RPM only
+            GetRPM();
+            break;
+        case 2:
+        {// lock/unlock only
+            int lock = !atoi(argv[1]);
+            if (argv[1][0] == '?' || argv[1][0] == '-' || argv[1][0] == '/' || lock < 0 || lock > 1) {
+                Usage();
+                break;
+            }
+            cout << "Setting lock " << lock << endl;
+            res = (ACPI_EVAL_OUTPUT_BUFFER*) EvalAcpiNSArgOutput((TCHAR*) TEXT("\\____SB_IETMMCHG"), (ACPI_EVAL_INPUT_BUFFER_COMPLEX*) PutIntArg(NULL, lock));
+            //if (res)
+            //    cout << "Lock Result " << hex << res->Argument[0].Argument << endl;
+            //else
+            //    cout << "Unlock operation failed." << endl;
+        } break;
+        case 4:
+        {// lock/unlock and set boost
+            UINT lock = !atoi(argv[1]);
+            UINT boost1 = atoi(argv[2]), boost2 = atoi(argv[3]);
+            if (argv[1][0] == '?' || argv[1][0] == '-' || argv[1][0] == '/' 
+                || boost1 < 0 || boost1 > 100
+                || boost2 < 0 || boost2 > 100
+                || lock < 0 || lock > 1) {
+                Usage();
+                break;
+            }
+            cout << "Setting lock " << lock << endl;
+            res = (ACPI_EVAL_OUTPUT_BUFFER*) EvalAcpiNSArgOutput((TCHAR*) TEXT("\\____SB_IETMMCHG"), (ACPI_EVAL_INPUT_BUFFER_COMPLEX*) PutIntArg(NULL, lock));
+            //if (res)
+            //    cout << "Lock Result " << hex << res->Argument[0].Argument << dec << endl;
+            //else
+            //    cout << "Unlock operation failed." << endl;
+            res = (PACPI_EVAL_OUTPUT_BUFFER) GetNSValue((TCHAR*) TEXT("\\____SB_AMW1RPM1"), &nType);
+            res2 = (PACPI_EVAL_OUTPUT_BUFFER) GetNSValue((TCHAR*) TEXT("\\____SB_AMW1RPM2"), &nType);
+            if (res && res2)
+                cout << "Current boost for FAN1 " << (UINT) res->Argument[0].Argument << ", FAN2 " << res2->Argument[0].Argument << endl;
+            ACPI_EVAL_INPUT_BUFFER_COMPLEX* args, * args2;
+            args = (ACPI_EVAL_INPUT_BUFFER_COMPLEX*) PutIntArg(NULL, 0x32);
+            args = (ACPI_EVAL_INPUT_BUFFER_COMPLEX*) PutIntArg(args, boost1);
+            args2 = (ACPI_EVAL_INPUT_BUFFER_COMPLEX*) PutIntArg(NULL, 0x33);
+            args2 = (ACPI_EVAL_INPUT_BUFFER_COMPLEX*) PutIntArg(args2, boost2);
+            res = (ACPI_EVAL_OUTPUT_BUFFER*) EvalAcpiNSArgOutput((TCHAR*) TEXT("\\____SB_AMW1SRPM"), args);
+            res2 = (ACPI_EVAL_OUTPUT_BUFFER*) EvalAcpiNSArgOutput((TCHAR*) TEXT("\\____SB_AMW1SRPM"), args2);
+            if (res && res2) {
+                cout << "Set boost for fans result " << hex << res->Argument[0].Argument << "," << res2->Argument[0].Argument << dec << endl;
+            }
+            res = (PACPI_EVAL_OUTPUT_BUFFER) GetNSValue((TCHAR*) TEXT("\\____SB_AMW1RPM1"), &nType);
+            res2 = (PACPI_EVAL_OUTPUT_BUFFER) GetNSValue((TCHAR*) TEXT("\\____SB_AMW1RPM2"), &nType);
+            if (res && res2)
+                cout << "New boost for FAN1 " << (UINT) res->Argument[0].Argument << ", FAN2 " << res2->Argument[0].Argument << endl;
+        } break;
+        default: // usage
+            Usage();
+        }
+    } else {
+        cout << "Can't load driver. Are you in Test mode and admin?" << endl;
+    }
+
+    cout << "Done!" << endl;
+    //CloseAcpiService(acc);
+}
